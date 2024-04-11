566db3e9734eb70b48ba1f70c13c6da971c6b582
Haleeva_Anastasia <haleevaanastas@gmail.com>
2024-04-11 06:36:16 +0400

    create allure-reports

--- src/test/java/org/ibs/steps/AddingFoodSteps.java
+++ src/test/java/org/ibs/steps/AddingFoodSteps.java
@@ -1,32 +1,38 @@
 package org.ibs.steps;
 
 import io.cucumber.java.ru.*;
+import io.qameta.allure.Step;
 import org.ibs.pages.FoodPage;
 import org.junit.jupiter.api.Assertions;
 
 public class AddingFoodSteps {
-    private FoodPage foodPage = FoodPage.getFoodPage();
+    private FoodPage foodPage;
 
+    @Step("Пользователь открывает страницу добавления продуктов")
     @Дано("пользователь открывает страницу добавления продуктов")
     public void пользовательОткрываетСтраницуДобавленияПродуктов() {
-        // Реализация открытия страницы добавления продуктов
+        foodPage = FoodPage.getFoodPage();
     }
 
+    @Step("Проверяем, что продукт {название} успешно добавлен")
     @Тогда("продукт {string} должен быть успешно добавлен")
     public void продуктДолженБытьУспешноДобавлен(String название) {
         Assertions.assertTrue(foodPage.containsProduct(название), "Не удалось добавить продукт: " + название);
     }
 
+    @Step("Очищаем данные")
     @Когда("пользователь очищает данные")
     public void пользовательОчищаетДанные() {
         foodPage.clearData();
     }
 
+    @Step("Проверяем, что список продуктов пустой")
     @Тогда("список продуктов должен быть пустым")
     public void списокПродуктовДолженБытьПустым() {
         Assertions.assertTrue(foodPage.checkTable(), "Не удалось очистить данные");
     }
 
+    @Step("Добавляем продукт: {название} с типом: {тип} и статусом: {статус}")
     @Когда("пользователь добавляет продукт {string} с типом {string} и статусом {string}")
     public void пользовательДобавляетПродуктСТипомИСтатусом(String название, String тип, String статус) {
         boolean status = Boolean.parseBoolean(статус);
